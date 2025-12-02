c2ccf58da784ab183b705f08afcb4218cbbbc29c
Aritra Debnath <aritradebnath1@gmail.com>
2025-12-02 22:22:44 +0530

    Add listing detail section

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,8 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { ClerkProvider } from "@clerk/clerk-react";
+import { Provider } from 'react-redux'
+import { store } from './app/store'
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -11,12 +13,14 @@ if (!PUBLISHABLE_KEY) {
 }
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <ClerkProvider
-      publishableKey={PUBLISHABLE_KEY}
-      appearance={{ variables: { colorPrimary: "#4f46e5" } }}
-    >
-      <App />
-    </ClerkProvider>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <ClerkProvider
+        publishableKey={PUBLISHABLE_KEY}
+        appearance={{ variables: { colorPrimary: "#4f46e5" } }}
+      >
+        <App />
+      </ClerkProvider>
+    </BrowserRouter>
+  </Provider>
 );
